132388f0a2ff7b6805b6ef860973d587273d36d1
leeyonghyun <mi161776@hanbat.ac.kr>
2019-05-04 00:49:48 +0900

    로그인 화면 구현
    - intro 페이지 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import '../styles/App.scss';
+import intro from '../static/images/intro.jpg';
+import Fab from '@material-ui/core/Fab';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={intro} alt="intro"/>
+        <div className="title">
+          <h1>Make your Album</h1>
+          <Fab color={"primary"} variant="extended" className="loginBtn">
+            Login
+          </Fab>
+          {/*<button className="loginBtn">*/}
+          {/*  Login*/}
+          {/*</button>*/}
+        </div>
       </div>
     );
   }
