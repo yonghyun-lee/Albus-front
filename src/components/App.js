6cab0cc98f171906801d933aa35f0e603c5619f1
leeyonghyun <mi161776@hanbat.ac.kr>
2019-05-04 21:15:05 +0900

    header 페이지에 nav 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import '../styles/App.scss';
-import intro from '../static/images/intro.jpg';
 import Fab from '@material-ui/core/Fab';
+import {Button} from "@material-ui/core";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={intro} alt="intro"/>
-        <div className="title">
-          <h1>Make your Album</h1>
-          <Fab color={"primary"} variant="extended" className="loginBtn">
-            Login
-          </Fab>
-          {/*<button className="loginBtn">*/}
-          {/*  Login*/}
-          {/*</button>*/}
+      <header>
+        <div className="container">
+          <nav>
+            <h1>Albus</h1>
+            <Button variant="outlined" color="secondary">Login</Button>
+          </nav>
+          <div className="title">
+            <h1>Make your Album.</h1>
+            <Fab color={"secondary"} variant="extended" className="loginBtn">
+              Start
+            </Fab>
+            {/*<button className="loginBtn">*/}
+            {/*  Login*/}
+            {/*</button>*/}
+          </div>
         </div>
-      </div>
+      </header>
     );
   }
 }
