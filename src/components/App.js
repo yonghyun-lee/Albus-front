16ab8becfd7935e4b6f78dd984b1811ad08e3559
leeyonghyun <mi161776@hanbat.ac.kr>
2019-05-06 21:01:13 +0900

    meta data 수정

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,11 @@ const App = () => {
   return (
     <React.Fragment>
       <Helmet>
-        <title>velog</title>
+        <title>Albus</title>
         <meta
           name="description"
-          content="개발자들을 위한 취향저격 블로그 서비스. 어디서 글 쓸지 고민하지 말고 벨로그에서 시작하세요."
+          content="추억을 담을 수 있는 나만의 앨범을 만들고 전시해보세요."
         />
-        <meta property="fb:app_id" content="203040656938507" />
       </Helmet>
       <Switch>
         <Route exact path="/" component={LoginPage} />
