742929556dd3efb566634ae8539db6c8358fc732
leeyonghyun <mi161776@hanbat.ac.kr>
2019-06-10 10:22:27 +0900

    Add react-full-page

--- src/components/MainTemplate/MainTemplate.js
+++ src/components/MainTemplate/MainTemplate.js
@@ -2,12 +2,19 @@ import React  from 'react';
 import './MainTemplate.scss';
 import ImageSliderContainer from "../../containers/ImageSliderContainer";
 import AlbumListContainer from "../../containers/AlbumListContainer";
+import { FullPage, Slide } from 'react-full-page';
 
 const MainTemplate = () => {
   return (
     <div className="mainTemplate">
-      <ImageSliderContainer class="snapScroll"/>
-      <AlbumListContainer class="snapScroll"/>
+      <FullPage>
+        <Slide>
+          <ImageSliderContainer/>
+        </Slide>
+        <Slide>
+          <AlbumListContainer/>
+        </Slide>
+      </FullPage>
     </div>
   );
 };
