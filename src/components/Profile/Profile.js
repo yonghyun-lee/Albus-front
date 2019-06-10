3b3e923d0a9ca55528748e5841d6bba20f401cfd
leeyonghyun <mi161776@hanbat.ac.kr>
2019-06-10 12:23:14 +0900

    Add profile nav button hover style

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,9 +2,14 @@ import React  from 'react';
 import './Profile.scss';
 
 const Profile = (props) => {
+
+  const profileNav = () => {
+    document.getElementById('profile-nav-menu')
+  };
+
   return (
     <div className="profile">
-      <img src={props.user.thumbnail} alt="thumbnail"/>
+      <img onClick={profileNav} src={props.user.thumbnail} alt="thumbnail"/>
       {/*<img src={props.thumbnail || defaultThumbnail} alt="thumbnail" />*/}
     </div>
   );
--- src/components/ProfileNavMenu/ProfileNavMenu.js
+++ src/components/ProfileNavMenu/ProfileNavMenu.js
@@ -6,15 +6,15 @@ const ProfileNavMenu = (props) => {
   const {email, username} = props.user;
 
   return (
-    <nav className="profile-nav-menu">
+    <nav id="profile-nav-menu">
       <div className="profile-menu-info">
         <p className="name">{username}</p>
         <p className="email">{email}</p>
       </div>
       <div>
         <ul>
-          <li><a href="#" onClick={props.logout}>Logout</a></li>
-          <li><a href="#">My Pictures</a></li>
+          <li><a href='#'>My Pictures</a></li>
+          <li><a href='#' onClick={props.logout}>Logout</a></li>
         </ul>
       </div>
     </nav>
