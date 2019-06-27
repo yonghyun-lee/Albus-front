6722e0ad5f62550c2f8b7a2f73ba9f2db0d88dad
leeyonghyun <mi161776@hanbat.ac.kr>
2019-06-27 21:13:40 +0900

    Add accept attribute in input && upload active, inactive implement

--- src/components/MyPicturePreview/MyPicturePreview.js
+++ src/components/MyPicturePreview/MyPicturePreview.js
@@ -2,9 +2,6 @@ import React  from 'react';
 import './MyPicturePreview.scss';
 
 const MyPicturePreview = (props) => {
-
-  console.log("uploadPicture", typeof props.uploadPicture, props.uploadPicture);
-  
   return (
     <div className="image-upload-preview">
       {props.uploadPicture.map((element, index)=> (
--- src/components/MyPictureUpload/MyPictureUpload.js
+++ src/components/MyPictureUpload/MyPictureUpload.js
@@ -7,10 +7,10 @@ const MyPictureUpload = (props) => {
   return (
     <div className="myPicture-upload">
       <div className="image-upload">
-        <input id="image-upload-input" onChange={props.uploadOnChange} type="file" multiple/>
+        <input id="image-upload-input" onChange={props.uploadOnChange} type="file" multiple accept="image/*"/>
         <label htmlFor="image-upload-input">Upload</label>
       </div>
-      <div className="image-upload-active">
+      <div className={props.uploadState? "image-upload-active": "image-upload-inactive"}>
         <MyPicturePreview uploadPicture={props.uploadPicture}/>
         <button>Save</button>
       </div>
